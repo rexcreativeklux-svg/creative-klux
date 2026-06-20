671ed320ca46bcea4efe879f638b78ab67ccd1de
Okpara202 <okparafavour202@gmail.com>
2026-06-20 15:08:52 +0100

    styling test

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,15 +4,25 @@ import { ReusableFunctionsProvider } from "@/context/ReusableFunctions";
 import { AuthProvider, useAuth } from "@/context/AuthContext";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import { Toaster } from "sonner";
-
+import Script from "next/script";
 
 export const metadata = {
   title: "Creative Klux",
-  description: "Creative Klux is the all-in-one platform for creators, managers & brands. Get stunning templates, AI-generated content, design tools, client dashboards, and monetization — all in one place.",
+  description:
+    "Creative Klux is the all-in-one platform for creators, managers & brands. Get stunning templates, AI-generated content, design tools, client dashboards, and monetization — all in one place.",
   icons: {
     icon: "/favicon.svg",
   },
 };
+
+{
+  /* Woxelo Live Chat */
+}
+<Script
+  src="https://app.woxelo.com/livechat/settings.js"
+  data-widget="6uqcR8AH5v77dtTXz1buTLJh7rWYGXs0eICS2n3s"
+  strategy="lazyOnload"
+/>;
 
 export default function RootLayout({ children }) {
   return (
@@ -20,7 +30,11 @@ export default function RootLayout({ children }) {
       <head>
         {/* Inter – variable font, super fast & beautiful */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
           rel="stylesheet"
@@ -31,15 +45,12 @@ export default function RootLayout({ children }) {
         <ThemeProvider>
           <AuthProvider>
             <BrandProvider>
-                <ReusableFunctionsProvider>
-                  {children}
-                </ReusableFunctionsProvider>
+              <ReusableFunctionsProvider>{children}</ReusableFunctionsProvider>
             </BrandProvider>
           </AuthProvider>
           <Toaster position="top-right" richColors closeButton />
         </ThemeProvider>
       </body>
     </html>
-
   );
 }
